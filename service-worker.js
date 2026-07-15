44fe404efc701455933e741b67ca3dd8b2160c7e
herfandbarrel88 <herfandbarrel@gmail.com>
2026-07-15 12:24:33 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "private-reserve-v8";
+const CACHE = "private-reserve-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
